61b4de04049ba70c2c793fb4d40b6fc6592814ae
Ibrahim <ibrahimsaqib20@gmail.com>
2024-11-25 20:08:44 +0500

    Changing Portfolio Link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/ibrahimsqb"
+                href="https://github.com/ibrahimsqb/Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
